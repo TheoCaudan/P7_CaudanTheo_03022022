150222015c67108ab443d69d25a265f897009b3b
Theo Caudan <theocaudan38@gmail.com>
2022-04-19 12:07:16 +0200

    test

--- js/recipesDisplay.js
+++ js/recipesDisplay.js
@@ -1,199 +1,266 @@
-import recipes from './recipes.js'
+import recipes from "./recipes.js";
 
-let currentRecipe = []
+let currentRecipe = [];
+/* let arrFiltered = []; */
 
-const ingredientsSet = new Set()
-const appliancesSet = new Set()
-const ustensilsSet = new Set()
+const ingredientsSet = new Set();
+const appliancesSet = new Set();
+const ustensilsSet = new Set();
 
-function search(key) {
-  currentRecipe = []
-  for(let i = 0; i < recipes.length; i++){
-    if(recipes[i].name.toLowerCase().includes(key.toLowerCase())){
-      currentRecipe.push(recipes[i])
-    } else if(recipes[i].description.toLowerCase().includes(key.toLowerCase())){
-      currentRecipe.push(recipes[i])
-    } else for(let j = 0; j < recipes[i].ingredients.length; j++){
-      if(recipes[i].ingredients[j].ingredient.toLowerCase().includes(key.toLowerCase())){
-        currentRecipe.push(recipes[i])
+/* let currentFilters = document.querySelector("#filterParams");
+let ingFilter = document.querySelectorAll('.filterParamIng');
+let appFilter = document.querySelectorAll('.filterParamApp');
+let ustFilter = document.querySelectorAll('.filterParamUst'); */
+
+let search = (key) => {
+  currentRecipe = [];
+  for (let i = 0; i < recipes.length; i++) {
+    if (recipes[i].name.toLowerCase().includes(key.toLowerCase())) {
+      currentRecipe.push(recipes[i]);
+    } else if (
+      recipes[i].description.toLowerCase().includes(key.toLowerCase())
+    ) {
+      currentRecipe.push(recipes[i]);
+    } else
+      for (let j = 0; j < recipes[i].ingredients.length; j++) {
+        if (
+          recipes[i].ingredients[j].ingredient
+            .toLowerCase()
+            .includes(key.toLowerCase())
+        ) {
+          currentRecipe.push(recipes[i]);
+        }
       }
-    }
   }
-  document.querySelector('.displayRecipes').innerHTML = ''
-  displayMain(currentRecipe)
-  filters(currentRecipe)
-  displayFilterOptions()
-}
+  /* filterOptions(currentRecipe); */
+  document.querySelector(".displayRecipes").innerHTML = "";
+  displayMain(currentRecipe);
+  filters(currentRecipe);
+  displayFilterOptions();
+};
 
-function filters(arr) {
+let filters = (arr) => {
   /* Get appliances, ustensils and ingredients converted to lower case and throw everything in Sets to prevent duplicates  */
-  ingredientsSet.clear()
-  ustensilsSet.clear()
-  appliancesSet.clear()
-  for(let k = 0; k < arr.length; k++){
+  ingredientsSet.clear();
+  ustensilsSet.clear();
+  appliancesSet.clear();
+  for (let k = 0; k < arr.length; k++) {
     for (let i = 0; i < arr[k].ingredients.length; i++) {
-      ingredientsSet.add(arr[k].ingredients[i].ingredient.toLowerCase())
+      ingredientsSet.add(arr[k].ingredients[i].ingredient.toLowerCase());
     }
     for (let i = 0; i < arr[k].ustensils.length; i++) {
-      ustensilsSet.add(arr[k].ustensils[i].toLowerCase())
+      ustensilsSet.add(arr[k].ustensils[i].toLowerCase());
     }
-    appliancesSet.add(arr[k].appliance.toLowerCase())
+    appliancesSet.add(arr[k].appliance.toLowerCase());
   }
-}
+};
 
-function displayMain(arr) {
-  for(let i = 0; i < arr.length; i++){
-    const recipesCard = document.createElement('article')
-    recipesCard.className = 'gridRecipesWrapper'
-    recipesCard.ariaLabel = 'Contenu Principal'
-    recipesCard.id = 'gridRecipesWrapper' + arr[i].id
+/* let filterOptions = (currentRecipe) => {
+  arrFiltered = [];
+  if (currentFilters.innerText) {
+    for(let m = 0; m < appFilter.length; m++) {
+      for (let i = 0; i < currentRecipe.length; i++) {
+        if (currentRecipe[i].appliance.toLowerCase().includes(appFilter[m].innerText)) {
+          arrFiltered.push(currentRecipe[i]);
+        } else
+          for (let j = 0; j < currentRecipe[i].ingredients.length; j++) {
+            if (
+              currentRecipe[i].ingredients[j].ingredient
+                .toLowerCase()
+                .includes(ingFilter[m].innerText)
+            ) {
+              arrFiltered.push(currentRecipe[i]);
+            } else
+              for (let j = 0; j < [i].ustensils.length; j++) {
+                if (
+                  arr[i].ustensils[j]
+                    .toLowerCase()
+                    .includes(ustFilter[m].innerText)
+                ) {
+                  arrFiltered.push(arr[i]);
+                }
+              }
+          }
+      }
+    }
+  } else
+    for (let i = 0; i < arr.length; i++) {
+      arrFiltered.push(arr[i]);
+    }
+}; */
 
-    displayArea.append(recipesCard)
+let displayMain = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const recipesCard = document.createElement("article");
+    recipesCard.className = "gridRecipesWrapper";
+    recipesCard.ariaLabel = "Contenu Principal";
+    recipesCard.id = "gridRecipesWrapper" + arr[i].id;
 
-    const recipe = document.createElement('div')
-    recipe.className = 'gridRecipeId'
-    recipe.id = 'gridRecipe' + arr[i].id
+    displayArea.append(recipesCard);
 
-    recipesCard.append(recipe)
+    const recipe = document.createElement("div");
+    recipe.className = "gridRecipeId";
+    recipe.id = "gridRecipe" + arr[i].id;
 
-    const recipeImg = document.createElement('img')
-    recipeImg.className = 'recipeImg'
-    recipeImg.id = 'recipeImg' + arr[i].id
+    recipesCard.append(recipe);
 
-    recipe.append(recipeImg)
+    const recipeImg = document.createElement("img");
+    recipeImg.className = "recipeImg";
+    recipeImg.id = "recipeImg" + arr[i].id;
 
-    const recipeTitle = document.createElement('h1')
-    recipeTitle.className = 'recipeName'
-    recipeTitle.textContent = '' + arr[i].name
-    recipeTitle.id = 'recipeName' + arr[i].id
+    recipe.append(recipeImg);
 
-    recipe.append(recipeTitle)
+    const recipeTitle = document.createElement("h1");
+    recipeTitle.className = "recipeName";
+    recipeTitle.textContent = "" + arr[i].name;
+    recipeTitle.id = "recipeName" + arr[i].id;
 
-    const recipeIngredients = document.createElement('div')
-    recipeIngredients.className = 'recipeIngredients'
-    recipeIngredients.id = 'recipeIngredients' + arr[i].id
+    recipe.append(recipeTitle);
 
-    recipe.append(recipeIngredients)
+    const recipeIngredients = document.createElement("div");
+    recipeIngredients.className = "recipeIngredients";
+    recipeIngredients.id = "recipeIngredients" + arr[i].id;
 
-    const subList = document.createElement('ul')
-    subList.className = 'listOfIngredients'
-    subList.id = 'listOfIngredients' + arr[i].id
+    recipe.append(recipeIngredients);
 
-    recipeIngredients.append(subList)
+    const subList = document.createElement("ul");
+    subList.className = "listOfIngredients";
+    subList.id = "listOfIngredients" + arr[i].id;
+
+    recipeIngredients.append(subList);
 
     for (let j = 0; j < arr[i].ingredients.length; j++) {
-      const subListItems = document.createElement('li')
+      const subListItems = document.createElement("li");
       if (arr[i].ingredients[j].unit) {
         subListItems.textContent =
-          arr[i].ingredients[j].ingredient + ' : ' +
-          arr[i].ingredients[j].quantity + ' ' +
-          arr[i].ingredients[j].unit
+          arr[i].ingredients[j].ingredient +
+          " : " +
+          arr[i].ingredients[j].quantity +
+          " " +
+          arr[i].ingredients[j].unit;
       } else if (arr[i].ingredients[j].quantity) {
         subListItems.textContent =
-          arr[i].ingredients[j].ingredient + ' : ' + arr[i].ingredients[j].quantity
+          arr[i].ingredients[j].ingredient +
+          " : " +
+          arr[i].ingredients[j].quantity;
       } else if (arr[i].ingredients[j].ingredient) {
-        subListItems.textContent = arr[i].ingredients[j].ingredient
+        subListItems.textContent = arr[i].ingredients[j].ingredient;
       }
-      subList.append(subListItems)
+      subList.append(subListItems);
     }
 
-    const recipeTime = document.createElement('span')
-    recipeTime.className = 'recipeTime'
+    const recipeTime = document.createElement("span");
+    recipeTime.className = "recipeTime";
 
-    recipeTime.innerHTML = '' + arr[i].time
-    recipeTime.id = 'recipeTime' + arr[i].id
+    recipeTime.innerHTML = "" + arr[i].time;
+    recipeTime.id = "recipeTime" + arr[i].id;
 
-    recipe.append(recipeTime)
+    recipe.append(recipeTime);
 
-    const recipeText = document.createElement('p')
-    recipeText.className = 'recipeText'
-    recipeText.textContent = '' + arr[i].description
-    recipeText.id = 'recipeText' + arr[i].id
+    const recipeText = document.createElement("p");
+    recipeText.className = "recipeText";
+    recipeText.textContent = "" + arr[i].description;
+    recipeText.id = "recipeText" + arr[i].id;
 
-    recipe.append(recipeText)
+    recipe.append(recipeText);
   }
-}
+};
 
-function displayFilterOptions() {
-  const ingredientsArray = Array.from(ingredientsSet)
-  const ingredientsFilter = document.querySelector('#ingredientsList')
+let displayFilterOptions = () => {
+  const ingredientsArray = Array.from(ingredientsSet);
+  const ingredientsFilter = document.querySelector("#ingredientsList");
 
-  ingredientsFilter.innerHTML = '<option value="" hidden selected>Ingredients</option>'
-  ingredientsFilter.onclick = function () {
-    const filterParams = document.querySelector('#filterParams')
-    if(ingredientsFilter.value) {
-      const filterParamIng = document.createElement('div')
-      filterParamIng.className = 'filterParamIng'
-      filterParamIng.textContent = ingredientsFilter.value
-      filterParamIng.onclick = function () {
-        this.remove()
-      }
-      filterParams.append(filterParamIng)
+  ingredientsFilter.innerHTML =
+    '<option value="" hidden selected>Ingredients</option>';
+  ingredientsFilter.onclick = () => {
+    const filterParams = document.querySelector("#filterParams");
+    if (ingredientsFilter.value) {
+      const filterParamIng = document.createElement("div");
+      filterParamIng.className = "filterParamIng";
+      filterParamIng.textContent = ingredientsFilter.value;
+      filterParamIng.onclick = () => {
+        this.remove();
+      };
+      filterParams.append(filterParamIng);
     }
-  }
-  
+  };
+
   for (let i = 0; i < ingredientsArray.length; i++) {
     ingredientsFilter.innerHTML +=
-      '<option value="'+ ingredientsArray[i] + '">' + ingredientsArray[i] + '</option>'
+      '<option value="' +
+      ingredientsArray[i] +
+      '">' +
+      ingredientsArray[i] +
+      "</option>";
   }
-  const appliancesArray = Array.from(appliancesSet)
-  const appliancesFilter = document.querySelector('#applianceList')
+  const appliancesArray = Array.from(appliancesSet);
+  const appliancesFilter = document.querySelector("#applianceList");
 
-  appliancesFilter.innerHTML = '<option value="" hidden selected>Appareils</option>'
-  appliancesFilter.onclick = function () {
-    const filterParams = document.querySelector('#filterParams')
-    if(appliancesFilter.value) {
-      const filterParamApp = document.createElement('div')
-      filterParamApp.className = 'filterParamApp'
-      filterParamApp.textContent = appliancesFilter.value
-      filterParamApp.onclick = function () {
-        this.remove()
-      }
-      filterParams.append(filterParamApp)
+  appliancesFilter.innerHTML =
+    '<option value="" hidden selected>Appareils</option>';
+  appliancesFilter.onclick = () => {
+    const filterParams = document.querySelector("#filterParams");
+    if (appliancesFilter.value) {
+      const filterParamApp = document.createElement("div");
+      filterParamApp.className = "filterParamApp";
+      filterParamApp.textContent = appliancesFilter.value;
+      filterParamApp.onclick = () => {
+        this.remove();
+      };
+      filterParams.append(filterParamApp);
     }
-  }
+  };
 
   for (let i = 0; i < appliancesArray.length; i++) {
     appliancesFilter.innerHTML +=
-      '<option value="' + appliancesArray[i] + '">' + appliancesArray[i] + '</option>'
+      '<option value="' +
+      appliancesArray[i] +
+      '">' +
+      appliancesArray[i] +
+      "</option>";
   }
-  const ustensilsArray = Array.from(ustensilsSet)
-  const ustensilsFilter = document.querySelector('#ustensilsList')
+  const ustensilsArray = Array.from(ustensilsSet);
+  const ustensilsFilter = document.querySelector("#ustensilsList");
 
-  ustensilsFilter.innerHTML = '<option value="" hidden selected>Ustensiles</option>'
-  ustensilsFilter.onclick = function () {
-    const filterParams = document.querySelector('#filterParams')
-    if(ustensilsFilter.value) {
-      const filterParamUst = document.createElement('div')
-      filterParamUst.className = 'filterParamUst'
-      filterParamUst.textContent = ustensilsFilter.value
-      filterParamUst.onclick = function () {
-        this.remove()
-      }
-      filterParams.append(filterParamUst)
+  ustensilsFilter.innerHTML =
+    '<option value="" hidden selected>Ustensiles</option>';
+  ustensilsFilter.onclick = () => {
+    const filterParams = document.querySelector("#filterParams");
+    if (ustensilsFilter.value) {
+      const filterParamUst = document.createElement("div");
+      filterParamUst.className = "filterParamUst";
+      filterParamUst.textContent = ustensilsFilter.value;
+      filterParamUst.onclick = () => {
+        this.remove();
+      };
+      filterParams.append(filterParamUst);
     }
-  }
+  };
 
   for (let i = 0; i < ustensilsArray.length; i++) {
     ustensilsFilter.innerHTML +=
-      '<option value="' + ustensilsArray[i] + '">' + ustensilsArray[i] + '</option>'
+      '<option value="' +
+      ustensilsArray[i] +
+      '">' +
+      ustensilsArray[i] +
+      "</option>";
   }
-}
+};
 
-const displayArea = document.querySelector('.displayRecipes')
+const displayArea = document.querySelector(".displayRecipes");
 
-displayMain(recipes)
-filters(recipes)
-displayFilterOptions()
+displayMain(recipes);
+filters(recipes);
+displayFilterOptions();
 
-document.querySelector('#searchengine').addEventListener('keyup', function (e) {
-  let val = e.target.value
+document.querySelector("#searchengine").addEventListener("keyup", function (e) {
+  let val = e.target.value;
   if (val.length > 2) {
-    search(val)
+    search(val);
   } else {
-    document.querySelector('.displayRecipes').innerHTML = ''
-    displayMain(recipes)
-    filters(recipes)
-    displayFilterOptions()
+    document.querySelector(".displayRecipes").innerHTML = "";
+    displayMain(recipes);
+    filters(recipes);
+    displayFilterOptions();
   }
-})
+});
